0c424dbca55648f241254ca58fb14d15390782e6
Istvan Acs <istvan.acs89@gmail.com>
2019-01-27 15:14:45 +0000

    jsx & new page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
-
+import React from 'react';
 
 
 const Index = () => {
   return (
-    <h1>I am Index Page</h1>
+    <h1>I am Index Page!</h1>
   )
 }
 
+// class Index extends React.Component {
+
+// }
 
 export default Index;
