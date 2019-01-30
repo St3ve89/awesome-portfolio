4404587551330cf7db8082db271fbbac493b954c
Istvan Acs <istvan.acs89@gmail.com>
2019-01-30 07:44:28 +0000

    Fetch posts from API

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
+import axios from 'axios';
+
 
 
 class Portfolios extends Component {
+  static async getInitialProps() {
+    let posts = {}
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      posts = response.data;
+    } catch(err) {
+      console.error(err);
+    }
+
+    return {posts: posts.splice(0,10)};
+  }
+
+  renderPosts(posts) {
+    return posts.map(post => {
+      return (
+        <li>{post.title}</li>
+      )
+    })
+  }
+
+
   render() {
+    const { posts } = this.props;
     return (
       <BaseLayout>
         <h1>i am portfolio page</h1>
+        <ul>
+          {this.renderPosts(posts)}
+        </ul>
       </BaseLayout>
     )
   }
