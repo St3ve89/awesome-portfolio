fe0ed9440ed025cf2bf367a139316b63dc63a1c6
Istvan Acs <istvan.acs89@gmail.com>
2019-02-09 08:15:57 +0000

    Callback page and redirect user

--- pages/callback.js
+++ pages/callback.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
 
+import auth0Client from '../services/auth0';
+import { withRouter } from 'next/router';
 
 class Callback extends Component {
+
+  async componentDidMount() {
+    await auth0Client.handleAuthentication();
+    this.props.router.push('/');
+  }
+
   render() {
     return (
       <BaseLayout>
@@ -15,4 +23,4 @@ class Callback extends Component {
   }
 }
 
-export default Callback
+export default withRouter(Callback);
