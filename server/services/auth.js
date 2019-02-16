3a638c460209967e2d4f0977a633bf5cd48ac594
Istvan Acs <istvan.acs89@gmail.com>
2019-02-16 16:15:10 +0000

    Working on middleware WIP

--- server/services/auth.js
+++ server/services/auth.js
@@ -1,12 +1,15 @@
-
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
 
 // MIDDLEWARE
-exports.checkJWT = function(req, res, next) {
-  const isValidToken = false;
-
-  if(isValidToken) {
-    next();
-  } else {
-    return res.status(401).send({title: 'Not Authorized', detail: 'Please login in order to get data'})
-  }
-}
+exports.checkJWT = jwt({
+  secret: jwksRsa({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinutes: 15,
+    jwksUri: 'https://dev-1cnzle42.eu.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'nwDpGURZPVEb70PsoPcu4VIjP6DzQrNr',
+  issuer: 'https://dev-1cnzle42.eu.auth0.com',
+  algorithms: ['RS256']
+})
