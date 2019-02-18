789f75973c83ded5638c4cd6c9e6157a0b464fee
Istvan Acs <istvan.acs89@gmail.com>
2019-02-18 22:27:37 +0000

    Start implementing forms to portfolio page

---
+++ pages/portfolioNew.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import BaseLayout from '../components/layouts/BaseLayout';
+import BasePage from '../components/BasePage';
+import withAuth from '../components/hoc/withAuth';
+import PortfolioForm from '../components/portfolios/PortfolioForm';
+
+class PortfolioNew extends Component {
+  render() {
+    return (
+      <BaseLayout {...this.props.auth}>
+        <BasePage className="portfolio-create-page" title="Create new Portfolio">
+          <PortfolioForm />
+        </BasePage>
+      </BaseLayout>
+    )
+  }
+}
+
+export default withAuth('siteOwner')(PortfolioNew);
