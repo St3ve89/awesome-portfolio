ffbad2c8cdda92111e577ddcfcfef2fc936816e5
Istvan Acs <istvan.acs89@gmail.com>
2019-02-18 20:56:57 +0000

    Styling portfolio cards

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
-import {Link} from '../routes';
-
-
+import { Card, CardText, CardBody,
+  CardTitle, Row, Col, CardHeader } from 'reactstrap';
 import axios from 'axios';
 
 
@@ -24,11 +23,21 @@ class Portfolios extends Component {
   renderPosts(posts) {
     return posts.map((post, index) => {
       return (
-        <li key={index}> 
-          <Link route={`/portfolio/${post.id}`}>
-            <a>{post.title}</a>
-          </Link>
-        </li>
+        <Col md="4">
+          <React.Fragment key={index}>
+            <span>
+              <Card className="portfolio-card">
+                <CardHeader className="portfolio-card-header">Some Position {index}</CardHeader>
+                <CardBody>
+                  <p className="portfolio-card-city"> Some Location {index} </p>
+                  <CardTitle className="portfolio-card-title">Some Company {index}</CardTitle>
+                  <CardText className="portfolio-card-text">Some Description {index}</CardText>
+                  <div className="readMore"> </div>
+                </CardBody>
+              </Card>
+            </span>
+          </React.Fragment>
+        </Col>
       )
     })
   }
@@ -38,10 +47,10 @@ class Portfolios extends Component {
     const { posts } = this.props;
     return (
       <BaseLayout {...this.props.auth}>
-        <BasePage title="Portfolios">
-          <ul>
+        <BasePage className="portfolio-page" title="Portfolios">
+          <Row>
             {this.renderPosts(posts)}
-          </ul>
+          </Row>
         </BasePage>
       </BaseLayout>
     )
