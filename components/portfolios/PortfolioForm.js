1f9d789e5f36c5cf21c65a3d43ed2480c909ca87
Istvan Acs <istvan.acs89@gmail.com>
2019-02-19 20:20:50 +0000

    Add more input types

--- components/portfolios/PortfolioForm.js
+++ components/portfolios/PortfolioForm.js
@@ -3,18 +3,19 @@ import React from 'react';
 export default class PortfolioForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {title: ''};
+    this.state = {title: '', description: '', language: ''};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({title: event.target.value});
+    const field = event.target.name;
+    this.setState({[field]: event.target.value});
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.title);
+    alert('A name was submitted: ' + this.state.title + ' ' + this.state.description + ' ' + this.state.language);
     event.preventDefault();
   }
 
@@ -23,7 +24,20 @@ export default class PortfolioForm extends React.Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input name="title" type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <label>
+          Description:
+          <textarea name="description" value={this.state.description} onChange={this.handleChange} />
+        </label>
+        <label>
+          Pick your favorite Programming Language:
+          <select name="language" value={this.state.language} onChange={this.handleChange}>
+            <option value="javascript">Javascript</option>
+            <option value="python">Python</option>
+            <option value="c++">C++</option>
+            <option value="c#">C#</option>
+          </select>
         </label>
         <input type="submit" value="Submit" />
       </form>
