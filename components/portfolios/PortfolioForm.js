e86c86f833653dc940d22af25c086dfaa6e40626
Istvan Acs <istvan.acs89@gmail.com>
2019-02-24 14:06:10 +0000

    Create datepicker component

--- components/portfolios/PortfolioForm.js
+++ components/portfolios/PortfolioForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import { Button, FormGroup, Label } from 'reactstrap';
 import PortfolioInput from '../form/PortfolioInput';
+import PortfolioDate from '../form/PortfolioDate';
 
 
 const validateInputs = (values) => {
@@ -46,8 +46,8 @@ const PortfolioForm = (props) => (
           <Field type="text" name="location" component={PortfolioInput} label="Location"/>
           <Field type="text" name="position" component={PortfolioInput} label="Position"/>
           <Field type="textarea" name="description" component={PortfolioInput} label="Description"/>
-          <Field type="text" name="startDate" component={PortfolioInput} label="Start Date"/>
-          <Field type="text" name="endDate" component={PortfolioInput} label="End Date"/>
+          <Field name="startDate" component={PortfolioDate} label="Start Date"/>
+          <Field name="endDate" component={PortfolioDate} label="End Date"/>
           <button type="submit" disabled={isSubmitting}>
             Create
           </button>
@@ -58,52 +58,3 @@ const PortfolioForm = (props) => (
 );
 
 export default PortfolioForm;
-
-
-
-// import React from 'react';
-
-// export default class PortfolioForm extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {title: '', description: '', language: ''};
-
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-
-//   handleChange(event) {
-//     const field = event.target.name;
-//     this.setState({[field]: event.target.value});
-//   }
-
-//   handleSubmit(event) {
-//     alert('A name was submitted: ' + this.state.title + ' ' + this.state.description + ' ' + this.state.language);
-//     event.preventDefault();
-//   }
-
-//   render() {
-//     return (
-//       <form onSubmit={this.handleSubmit}>
-//         <label>
-//           Name:
-//           <input name="title" type="text" value={this.state.value} onChange={this.handleChange} />
-//         </label>
-//         <label>
-//           Description:
-//           <textarea name="description" value={this.state.description} onChange={this.handleChange} />
-//         </label>
-//         <label>
-//           Pick your favorite Programming Language:
-//           <select name="language" value={this.state.language} onChange={this.handleChange}>
-//             <option value="javascript">Javascript</option>
-//             <option value="python">Python</option>
-//             <option value="c++">C++</option>
-//             <option value="c#">C#</option>
-//           </select>
-//         </label>
-//         <input type="submit" value="Submit" />
-//       </form>
-//     );
-//   }
-// }
