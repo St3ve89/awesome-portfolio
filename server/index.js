352f335042183e84d3cf3471fa237c9caa54112b
Istvan Acs <istvan.acs89@gmail.com>
2019-03-06 21:24:14 +0000

    Books routes and controllers and some refactoring

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ const config = require('./config');
 
 const Book = require('./models/books');
 const bodyParser = require('body-parser');
+const bookRoutes = require('./routes/book');
 
 const secretData = [
   {
@@ -34,61 +35,7 @@ app.prepare()
   const server = express();
   server.use(bodyParser.json());
 
-  server.post('/api/v1/books', (req, res) => {
-    const bookData = req.body;
-
-    const book = new Book(bookData);
-
-    book.save((err, createdBook) => {
-      if(err) {
-        return res.status(422).send(err);
-      } else {
-        return res.json(createdBook);
-      }
-    });
-  });
-
-  server.get('/api/v1/books', (req, res) => {
-    Book.find({}, (err, allBooks) => {
-      if(err) {
-        return res.status(422).send(err);
-      } else {
-        return res.json(allBooks);
-      }
-    })
-  });
-
-  server.patch('/api/v1/books/:id', (req, res) => {
-    const bookId = req.params.id;
-    const bookData = req.body;
-
-    Book.findById(bookId, (err, foundBook) => {
-      if(err) {
-        return res.status(422).send(err);
-      } else {
-        foundBook.set(bookData);
-        foundBook.save((err, savedBook) => {
-          if(err) {
-            return res.status(422).send(err);
-          } else {
-            return res.json(foundBook);
-          }
-        })
-      }
-    })
-  });
-
-  server.delete('/api/v1/books/:id', (req, res) => {
-    const bookId = req.params.id;
-
-    Book.deleteOne({_id: bookId}, (err, deletedBook) => {
-      if(err) {
-        return res.status(422).send(err);
-      } else {
-        return res.json({status: 'DELETED'});
-      }
-    })
-  });
+  server.use('/api/v1/books', bookRoutes);
 
   server.get('/api/v1/secret', authService.checkJWT, (req, res) => {
     return res.json(secretData);
