6a7ab75c0e9e0a411aa4322f626e071db45fffc4
Istvan Acs <istvan.acs89@gmail.com>
2019-04-28 20:56:55 +0100

    Delete and get published blogs on the server

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -5,6 +5,7 @@ const authService = require('../services/auth');
 
 const blogCtrl = require('../controllers/blog');
 
+router.get('', blogCtrl.getBlogs);
 
 router.get('/me', authService.checkJWT, authService.checkRole('siteOwner'), blogCtrl.getUserBlogs);
 
@@ -13,6 +14,8 @@ router.get('/:id', blogCtrl.getBlogById);
 router.post('', authService.checkJWT, authService.checkRole('siteOwner'), blogCtrl.createBlog);
 
 router.patch('/:id', authService.checkJWT, authService.checkRole('siteOwner'), blogCtrl.updateBlog);
+
+router.delete('/:id', authService.checkJWT, authService.checkRole('siteOwner'), blogCtrl.deleteBlog);
 
 
 module.exports = router;
