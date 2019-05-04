3ce8bbaf26e18fc0d148ffcd6e11bb856f6f97ea
Istvan Acs <istvan.acs89@gmail.com>
2019-05-04 15:47:58 +0100

    Styling about page

--- pages/about.js
+++ pages/about.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
 
+import { Row, Col } from 'reactstrap';
 class About extends Component {
   render() {
     return (
       <BaseLayout {...this.props.auth}>
-        <BasePage className="about-page" title="about page">
-          
+        <BasePage className="about-page">
+          <Row className="mt-5">
+            <Col md="6">
+              <div className="left-side">
+                <h1 className="title fadein">Hello, Welcome</h1>
+                <h4 className="subtitle fadein">To About Page</h4>
+                <p className="subsubTitle fadein">Feel free to read short description about me.</p>
+              </div>
+            </Col>
+            <Col md="6">
+              <div className="fadein">
+                <p>My name is Istvan Acs and I make the front-end world better every day.</p>
+                <p>I’m a creative self-taught professional and I build in the Front-end “amazing boats”, so users can surf a better web. I design fluid and responsive websites for the future. Let’s have a chat!</p>
+              </div>
+            </Col>
+          </Row>
         </BasePage>
       </BaseLayout>
     )
